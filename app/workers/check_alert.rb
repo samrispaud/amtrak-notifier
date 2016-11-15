048a38c01f6442e662e2f342f422ae15ed431835
Sam Rispaud <samrispaud@gmail.com>
2016-11-15 00:49:45 -0500

    Fix

--- app/workers/check_alert.rb
+++ app/workers/check_alert.rb
@@ -5,6 +5,7 @@ class CheckAlert < ActiveJob::Base
     alert = Alert.find(alert_id)
     current_price = GoogleFinance::Api.quote(alert.exchange, alert.ticker).to_f
     alert.update(last_price: current_price)
+    msg = "#{alert.order.order_type} #{alert.order.quantity} shares of #{alert.order.ticker} ($#{alert.last_price} #{alert.comparison_logic} $#{alert.price})"
     case alert.comparison_logic
     when "GREATER THAN"
       if (current_price > alert.price)
@@ -13,13 +14,15 @@ class CheckAlert < ActiveJob::Base
         if stockfuse.errors.present?
           alert.order.update(status: stockfuse.errors.join(","))
           alert.update(status: "Error")
+          msg = "[ERROR] " + msg
           @client = Twilio::REST::Client.new
-          @client.messages.create( from: ENV["TWILIO_NUMBER"], to: alert.user.phone_number, body: "[ERROR] #{alert.order.order_type} #{alert.order.quantity} shares of #{alert.order.ticker} ($#{alert.last_price} #{alert.comparison_logic} $#{alert.price})" )
+          @client.messages.create( from: ENV["TWILIO_NUMBER"], to: alert.user.phone_number, body: msg )
         else
           alert.order.update(status: "Success")
           alert.update(status: "Success")
+          msg = "[SUCCESS] " + msg
           @client = Twilio::REST::Client.new
-          @client.messages.create( from: ENV["TWILIO_NUMBER"], to: alert.user.phone_number, body: "[SUCCESS] #{alert.order.order_type} #{alert.order.quantity} shares of #{alert.order.ticker} ($#{alert.last_price} #{alert.comparison_logic} $#{alert.price})" )
+          @client.messages.create( from: ENV["TWILIO_NUMBER"], to: alert.user.phone_number, body: msg )
         end
       end
     when "LESS THAN"
@@ -29,13 +32,15 @@ class CheckAlert < ActiveJob::Base
         if stockfuse.errors.present?
           alert.order.update(status: stockfuse.errors.join(","))
           alert.update(status: "Error")
+          msg = "[ERROR] " + msg
           @client = Twilio::REST::Client.new
-          @client.messages.create( from: ENV["TWILIO_NUMBER"], to: alert.user.phone_number, body: "[ERROR] #{alert.order.order_type} #{alert.order.quantity} shares of #{alert.order.ticker} ($#{alert.last_price} #{alert.comparison_logic} $#{alert.price})" )
+          @client.messages.create( from: ENV["TWILIO_NUMBER"], to: alert.user.phone_number, body: msg )
         else
           alert.order.update(status: "Success")
           alert.update(status: "Success")
+          msg = "[SUCCESS] " + msg
           @client = Twilio::REST::Client.new
-          @client.messages.create( from: ENV["TWILIO_NUMBER"], to: alert.user.phone_number, body: "[SUCCESS] #{alert.order.order_type} #{alert.order.quantity} shares of #{alert.order.ticker} ($#{alert.last_price} #{alert.comparison_logic} $#{alert.price})" )
+          @client.messages.create( from: ENV["TWILIO_NUMBER"], to: alert.user.phone_number, body: msg )
         end
       end
     end
